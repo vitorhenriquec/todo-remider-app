088d9f70359142c8ce6bf44016fcb99aa6076d71
Vitor Henrique <vitorhenriquec@users.noreply.github.com>
2020-02-07 17:39:38 -0300

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core"
+
+@Component({
+    selector: "ns-home",
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.css"],
+    moduleId: module.id
+})
+
+export class HomeComponent{
+    task = ''
+}
